b2958ff3537390a5cdfaa5fa40e30e98cca48230
drew-moore <44drewmoore@gmail.com>
2024-12-14 20:22:45 -0500

    Initial commit

---
+++ apps/subversive/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'subversive',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/subversive',
+};
